dcf07c0b05d706d91d85122b5f0cb28f3a92fe0a
Didier DONSEZ <didier.donsez@gmail.com>
2025-04-30 20:25:21 +0200

    add simple TX RX for radiolib with SX1280 Lambda80

---
+++ Firmware/Arduino/RadioLib/SX128x/SX128x_Transmit_Blocking/Settings.h
@@ -0,0 +1,9 @@
+/*******************************************************************************************************
+  Programs for Arduino - Copyright of the author Stuart Robinson - 02/03/20
+
+  This program is supplied as is, it is up to the user of the program to decide if the program is
+  suitable for the intended purpose and free from errors.
+*******************************************************************************************************/
+
+#include "SettingsModule.h"
+#include "SettingsModulation.h"
