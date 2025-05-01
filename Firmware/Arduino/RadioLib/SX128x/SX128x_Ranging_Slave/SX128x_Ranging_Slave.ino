75cab74fab58972ac5be47df0f5de7ca4089c06a
Didier DONSEZ <didier.donsez@gmail.com>
2025-05-01 08:23:12 +0200

    add debug for RL; change default SF

--- Firmware/Arduino/RadioLib/SX128x/SX128x_Ranging_Slave/SX128x_Ranging_Slave.ino
+++ Firmware/Arduino/RadioLib/SX128x/SX128x_Ranging_Slave/SX128x_Ranging_Slave.ino
@@ -18,6 +18,20 @@
   https://jgromes.github.io/RadioLib/
 */
 
+/*
+ * Debug output enable.
+ * Warning: Debug output will slow down the whole system significantly.
+ *          Also, it will result in larger compiled binary.
+ * Levels: basic - only main info
+ *         protocol - mainly LoRaWAN stuff, but other protocols as well
+ *         SPI - full transcript of all SPI communication
+ */
+#define RADIOLIB_DEBUG_BASIC          (1)
+#define RADIOLIB_DEBUG_PROTOCOL       (1)
+#define RADIOLIB_DEBUG_SPI            (1)
+#define RADIOLIB_VERBOSE_ASSERT       (1)
+// #define RADIOLIB_DEBUG_PORT           Serial // Serial /stdout
+
 // include the library
 #include <RadioLib.h>
 
@@ -50,7 +64,7 @@ void setup() {
   Serial.print(F("[SX1280] Initializing ... "));
 
   // Use default modulation parameters 
-  int state =  radio.begin(Frequency, Bandwidth, SpreadingFactor, CodeRate, SyncWord, TxPower, PreambleLenInSymb);
+  int state =  radio.begin(Frequency, Bandwidth, 5, CodeRate, SyncWord, TxPower, PreambleLenInSymb);
 
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
